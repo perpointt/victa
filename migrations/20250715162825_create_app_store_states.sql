b401709e184906dfd7f9f56600f7c0dbaf0b361b
Maxim Ilyshchenko <per.pointt@gmail.com>
2025-07-15 20:06:00 +0300

    feat: добавил новые миграции для сторов

---
+++ migrations/20250715162825_create_app_store_states.sql
@@ -0,0 +1,20 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE app_store_states
+(
+    app_id         BIGINT      NOT NULL REFERENCES apps (id) ON DELETE CASCADE,
+    store_id       BIGINT      NOT NULL REFERENCES stores (id) ON DELETE CASCADE,
+    last_version   BIGINT NULL,
+    last_review_id BIGINT NULL,
+    updated_at     TIMESTAMP NOT NULL DEFAULT now(),
+    PRIMARY KEY (app_id, store_id)
+);
+
+CREATE INDEX idx_app_store_states_store_id ON app_store_states (store_id);
+-- +goose StatementEnd
+
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS app_store_states;
+-- +goose StatementEnd
